a6914dd2b31f5960b0c0e952af222ee660a48e36
Lucas Malatchoumy <malatchoumylucas.com>
2023-12-22 10:42:13 +0100

    feat(Index): clean syntaxt

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json())
 app.use(cors({
-    origin: 'https://swipet.vercel.app',
+    origin: process.env.FRONT_URL,
     optionsSuccessStatus: 200
   }));
   
 
-// Configure Sendinblue API client
 let defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 let apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = process.env.API_KEY;
 
-// Create a route to handle the email submission
 app.post('/subscribe', (req, res) => {
   const { email } = req.body;
-  console.log(email);
   let apiInstance = new SibApiV3Sdk.ContactsApi();
 
 let createContact = new SibApiV3Sdk.CreateContact();
@@ -30,10 +27,9 @@ let createContact = new SibApiV3Sdk.CreateContact();
   apiInstance.createContact(createContact)
     .then(data => {
       console.log('Contact added successfully:', data);
-      res.status(200).json({ message: 'Contact added successfully' });
+      return res.status(200).json({ message: 'Contact added successfully' });
     })
     .catch(error => {
-      console.error('Error adding contact:', error);
       res.status(500).json({ error: 'Failed to add contact' });
     });
 });
